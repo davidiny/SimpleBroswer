68189f588a6c0eca2b6db948f144f198733f6ea0
David Inyangson <dinyangs@andrew.cmu.edu>
2019-09-15 14:13:04 -0400

    Initial Commit

---
+++ SimpleBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by David Inyangson on 9/15/19.
+//  Copyright © 2019 David Inyangson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
